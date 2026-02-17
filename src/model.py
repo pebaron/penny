6bb8b1645b74cc3ab3d6f71f329a59ec64d3bd1a
Petr Baron <petr.baron@cern.ch>
2026-02-17 12:44:28 +0100

    demo_v2

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 import joblib
+from pathlib import Path
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -14,9 +15,15 @@ def train_eval_save(df: pd.DataFrame, feature_cols, target_col: str, test_size: 
 
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train)
+    
 
     y_pred = model.predict_proba(X_test)[:, 1]
     auc = roc_auc_score(y_test, y_pred)
+
+    # before joblib.dump(...)
+    model_path = Path(model_path)
+    model_path.parent.mkdir(parents=True, exist_ok=True)
+
 
     joblib.dump(model, model_path)
 
